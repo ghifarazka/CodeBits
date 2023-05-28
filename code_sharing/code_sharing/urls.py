c3281f4542c6ecb44d020a13cc1a10f4624d1162
azukagi-haru <GhifarAzka21@gmail.com>
2023-05-28 09:36:42 +0700

    deleted files, replacing [ganti nama app, gabungin front-end]

--- code_sharing/code_sharing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import CodeCreateView
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('index/', views.index, name='index'),
-    path('code/new/', CodeCreateView.as_view(), name='code-create')
-]
--- code_sharing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home-Code'),
-    path('mycodes/', views.mycodes, name='myCode-Code'),
-]
